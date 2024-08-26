1d583de0e38867e7d7ec992dd6a36ac496459f2b
Leandro Alfaro <leandro@netegia.com>
2024-08-26 20:08:04 -0300

    cosito que va arriba y abajo fix

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -21,28 +21,29 @@ const Tech = () => {
         ))}
       </div>
 
-      <div style={{top: '90vh'}}
-          className="absolute xs:bottom-10 bottom-32 w-full 
-          flex justify-center items-center">
-          <a href="#about">
-            <div
-              className="w-[35px] h-[64px] rounded-3xl border-4 
-            border-french border-dim flex
-            justify-center items-start p-2">
-              <motion.div
-                animate={{
-                  y: [0, 24, 0],
-                }}
-                transition={{
-                  duration: 1.5,
-                  repeat: Infinity,
-                  repeatType: 'loop',
-                }}
-                className="w-3 h-3 rounded-full bg-taupe mb-1"
-              />
-            </div>
-          </a>
-        </div>
+      <div
+        className="absolute bottom-[-10rem] w-full 
+        flex justify-center items-center">
+        <a href="#about">
+          <div
+            className="w-[35px] h-[64px] rounded-3xl border-4 
+              border-french border-dim flex
+              justify-center items-start p-2">
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop',
+              }}
+              className="w-3 h-3 rounded-full bg-taupe mb-1"
+            />
+          </div>
+        </a>
+      </div>
+
     </>
   );
 };

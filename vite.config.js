1c2b717d8e5e0ba60fb7e47e113c99dae98f2cd9
Leandro <being.leeaa15@gmail.com>
2024-08-26 09:00:21 -0300

    deploy andando

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio3/', 
+  base: '/', 
+  build: {
+    rollupOptions: {
+      output: {
+        // Generar todos los archivos en la raíz del directorio de salida
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+        // Colocar los assets (imágenes, fuentes, etc.) en la raíz del directorio de salida
+        dir: 'dist'
+      }
+    }
+  }
 })

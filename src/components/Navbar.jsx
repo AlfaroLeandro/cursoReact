950dd35945c0136f92958f5759aee235769ffeca
Leandro Alfaro <leandro@netegia.com>
2024-08-26 20:02:09 -0300

    navbar fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,8 +12,7 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`${styles.paddingX} w-full flex items-center py-2 fixed 
-      top-0 z-20 bg-flashWhite ${styles.color0} sm:opacity-[0.97] xxs:h-[12vh]`}>
+      className={`${styles.paddingX} w-full flex items-center py-2 fixed top-0 z-20 bg-flashWhite ${styles.color0} sm:opacity-[0.97] xxs:h-[12vh]`}>
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
           to="/"
@@ -22,28 +21,22 @@ const Navbar = () => {
             setActive('');
             window.scrollTo(0, 0);
           }}>
-          {/* <img
-            src={logo} // your logo comes here
-            alt="logo"
-            className="sm:w-[50px] sm:h-[50px] w-[45px] h-[45px] object-contain"
-          /> */}
-
-          {/* if you have text you want besides your logo it comes here.
-          Otherwise delete this if you don't need it. */}
+          {/* Logo or Text */}
           <img
             src={logotext}
             alt="logo"
             className="sm:w-[90px] sm:h-[90px] w-[100px] h-[100px] -ml-[-0.5rem] object-contain"
           />
         </Link>
+
+        {/* Desktop Menu */}
         <ul className="list-none hidden sm:flex flex-row gap-8 mt-2 items-center">
           {navLinks.map((nav) => (
             <li
               key={nav.id}
               className={`${
                 active === nav.title ? 'text-french' : 'text-eerieBlack'
-              } hover:text-taupe text-[21px] font-medium font-PermanentMarker 
-                uppercase tracking-[3px] cursor-pointer nav-links`}
+              } hover:text-taupe text-[21px] font-medium font-PermanentMarker uppercase tracking-[3px] cursor-pointer nav-links`}
               onClick={() => setActive(nav.title)}>
               <a href={`#${nav.id}`}>{nav.title}</a>
             </li>
@@ -65,40 +58,47 @@ const Navbar = () => {
           </li>
         </ul>
 
-        {/* mobile */}
-        <div className="sm:hidden flex flex-1 w-screen justify-end items-center">
+        {/* Mobile Menu */}
+        <div className="sm:hidden flex flex-1 justify-end items-center">
           {toggle ? (
             <div
-              className={`p-6 bg-flashWhite opacity-[0.98] absolute 
-                top-0 left-0 w-screen h-[100vh] z-10 menu ${
-                  toggle ? 'menu-open' : 'menu-close'
-                }`}>
+              className={`p-6 bg-flashWhite opacity-[0.98] absolute top-0 left-0 w-full z-10`}>
+              {/* Close Button */}
               <div className="flex justify-end">
                 <img
                   src={close}
                   alt="close"
                   className="w-[22px] h-[22px] object-contain cursor-pointer"
-                  onClick={() => setToggle(!toggle)}
+                  onClick={() => setToggle(false)}
                 />
               </div>
-              <ul
-                className="list-none flex flex-col -gap-[1rem] 
-                items-start justify-end mt-[10rem] -ml-[35px]">
+              <ul className="list-none flex flex-col gap-4 items-start mt-10 ml-6">
                 {navLinks.map((nav) => (
                   <li
                     id={nav.id}
                     key={nav.id}
                     className={`${
                       active === nav.title ? 'text-french' : 'text-eerieBlack'
-                    } text-[88px] font-bold font-arenq 
-                      uppercase tracking-[1px] cursor-pointer`}
+                    } text-[24px] font-bold uppercase tracking-[1px] cursor-pointer`}
                     onClick={() => {
-                      setToggle(!toggle);
+                      setToggle(false);
                       setActive(nav.title);
                     }}>
                     <a href={`#${nav.id}`}>{nav.title}</a>
                   </li>
                 ))}
+                <li className="flex gap-5">
+
+                  <a target="_blank" href="https://github.com/AlfaroLeandro">
+                    <AiFillGithub size="3rem" />
+                  </a>
+                  <a target="_blank" href="https://wa.me/1132553928?text=%C2%A1Hola!%20Estoy%20interesado%20en%20tu%20servicio%20como%20desarrollador!">
+                    <FaWhatsapp size="3rem" />
+                  </a>
+                  <a target="_blank" href="https://www.linkedin.com/in/leandro-alfaro-547864233">
+                    <AiFillLinkedin size="3rem" />
+                  </a>
+                </li>
               </ul>
             </div>
           ) : (
@@ -106,7 +106,7 @@ const Navbar = () => {
               src={menu}
               alt="menu"
               className="w-[34px] h-[34px] object-contain cursor-pointer"
-              onClick={() => setToggle(!toggle)}
+              onClick={() => setToggle(true)}
             />
           )}
         </div>
